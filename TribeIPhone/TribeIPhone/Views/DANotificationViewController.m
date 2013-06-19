50353e4a774efacf8779600cecf66fe458448295
LI LIN <r2space@gmail.com>
2013-06-19 16:26:00 +0800

    #70 通知_各一览画面的调整

--- TribeIPhone/TribeIPhone/Views/DANotificationViewController.m
+++ TribeIPhone/TribeIPhone/Views/DANotificationViewController.m
@@ -12,27 +12,15 @@
 
 @interface DANotificationViewController ()
 {
-    int _content;
+    NotificationType _content;  // 通知的种类
     NSArray *_notifications;
-    NSArray *theUsers;
-    int user_count;
-    
-    
 }
 
 @end
 
 @implementation DANotificationViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
+// 返回按钮
 - (IBAction)onCancelClicked:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -41,186 +29,113 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _content = 2;
-    [[DANotificationModule alloc] getNotificationListByType:@"at" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
+
+    // 高亮显示第一个Tab
+    [self.tabBar setSelectedItem:[self.tabBar.items objectAtIndex:0]];
+
+    // 获取[提到我]
+    _content = NotificationTypeAt;
+    [[DANotificationModule alloc] getNotificationListByType:@"at" start:0 count:20
+                                                   callback:^(NSError *error, DANotificationList *notificationList){
         _notifications = notificationList.items;
         [self.tableView reloadData];
     }];
+    
+    // 显示通知件数
     UITabBarItem *itemMail = [ self.tabBar.items objectAtIndex:2];
     itemMail.badgeValue = @"2";
-    self.navigationController.tabBarItem.badgeValue=@"12";
-    //向下的收拾 返回上一页
-    UISwipeGestureRecognizer *oneFingerSwipeUp =
-    [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(oneFingerSwipeUp:)] ;
-    [oneFingerSwipeUp setDirection:UISwipeGestureRecognizerDirectionRight];
-    [[self view] addGestureRecognizer:oneFingerSwipeUp];
-    
-    
-    // Do any additional setup after loading the view from its nib.
-}
-- (void)oneFingerSwipeUp:(UISwipeGestureRecognizer *)recognizer
-{
-    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-// Sent to the delegate when the user selects a tab bar item.
-
+// TabBar选择事件
 - (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
 {
-    // UITabBarItem 'tag' value is used to identify the saved tab bar item
-    // Save current tab bar item to the defaults system.
-    NSLog(@"dfdfd%d ",item.tag);
-    switch (item.tag) {
-        case 0:
+    _content = item.tag;
+    
+    switch (_content) {
+        case NotificationTypeAt:// 提到我
         {
-            _content = 1;
-            [[DANotificationModule alloc] getNotificationListByType:@"reply" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
-                _notifications = notificationList.items;
-                [self.tableView reloadData];
-            }];
-        }
-            break;
-        case 1:
-        {
-            _content = 2;
+            _content = NotificationTypeAt;
             [[DANotificationModule alloc] getNotificationListByType:@"at" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
                 _notifications = notificationList.items;
                 [self.tableView reloadData];
             }];
-        }
             break;
-        case 2:
+        }
+        case NotificationTypeReply:// 评论
         {
-            _content = 3;
-            [[DAShortmailModule alloc] getUsers:0 count:20 callback:^(NSError *error, DAContactList *contact){
-                theUsers = contact.items;
-                user_count = contact.items.count;
+            _content = NotificationTypeReply;
+            [[DANotificationModule alloc] getNotificationListByType:@"reply" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
+                _notifications = notificationList.items;
                 [self.tableView reloadData];
             }];
-        }
-            NSLog(@"私信");
             break;
-        case 3:
+        }
+        case NotificationTypePrivateMessage:// 私信
         {
-            _content = 4;
+            _content = NotificationTypePrivateMessage;
+            [[DAShortmailModule alloc] getUsers:0 count:20 callback:^(NSError *error, DAContactList *contact){
+                _notifications = contact.items;
+                [self.tableView reloadData];
+            }];
+            break;
+        }
+        case NotificationTypeSystemAlert:// 通知
+        {
             [[DANotificationModule alloc] getNotificationListByType:@"invite,remove,follow" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
                 _notifications = notificationList.items;
                 [self.tableView reloadData];
             }];
+            break;
         }
-            break;
         default:
-            NSLog(@"更多");
+        {
             break;
+        }
     }
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (_content ==3 ) {
-        return user_count;
-    }else{
-        return _notifications.count;
-    }
-    
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _notifications.count;
 }
 
 - (CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-    if (_content == 2) {
-        DANotification *notification = [_notifications objectAtIndex:indexPath.row];
-        return [DAMessageCell cellHeightWithMessage:notification.message];
-    }
-    
-    return 55;
+    return 44;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (_content == 3) {
-        DAContact *contact = [theUsers objectAtIndex:indexPath.row];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (_content == NotificationTypePrivateMessage) {
+        DAContact *contact = [_notifications objectAtIndex:indexPath.row];
         DAShortmailViewCell *cells = [DAShortmailViewCell initWithMessage:contact  tableView:tableView];
         cells.lblName.text = contact.user.name.name_zh;
         return cells;
     }
     
-    
-    static NSString *identifier1 = @"DAMessageViewCellComment";
-    //    static NSString *identifier2 = @"DAMessageViewCellAt";
-    //    static NSString *identifier3 = @"DAMessageViewCellMail";
-    
-	UITableViewCell *cell;
-    
     DANotification *notification = [_notifications objectAtIndex:indexPath.row];
-    switch (_content) {
-        case 1:
-            cell = [DANotificationCell initWithNotification:notification tableView:tableView];
-            break;
-        case 2:
-            cell = [DAMessageCell initWithMessage:notification.message tableView:tableView];
-            break;
-        case 3:
-            
-            
-            
-            break;
-        case 4:
-            cell = [DANotificationCell initWithNotification:notification tableView:tableView];
-            break;
-            
-        default:
-            cell = [tableView dequeueReusableCellWithIdentifier:identifier1];
-            break;
-    }
-    
-    
+    UITableViewCell *cell = [DANotificationCell initWithNotification:notification tableView:tableView];
     return cell;
-    
 }
 
-
-- (IBAction)onCommentClicked:(id)sender {
-    _content = 1;
-    [[DANotificationModule alloc] getNotificationListByType:@"reply" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
-        _notifications = notificationList.items;
-        [self.tableView reloadData];
-    }];
-}
-
-- (IBAction)onAtClicked:(id)sender {
-    _content = 2;
-    [[DANotificationModule alloc] getNotificationListByType:@"at" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
-        _notifications = notificationList.items;
-        [self.tableView reloadData];
-    }];
-}
-
-- (IBAction)onMailClicked:(id)sender {
-    
-    [[DAShortmailModule alloc] getUsers:0 count:20 callback:^(NSError *error, DAContactList *contact){
-        theUsers = contact.items;
-        user_count = contact.items.count;
-        _content = 3;
-        [self.tableView reloadData];
-    }];
-    
-}
-
-- (IBAction)onAlertClicked:(id)sender {
-    _content = 4;
-    [[DANotificationModule alloc] getNotificationListByType:@"invite,remove,follow" start:0 count:20 callback:^(NSError *error, DANotificationList *notificationList){
-        _notifications = notificationList.items;
-        [self.tableView reloadData];
-    }];
-}
-
-- (void)didReceiveMemoryWarning
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (_content == NotificationTypePrivateMessage) {
+        DAShortmailStoryViewController *shortmailStoryViewController =[[DAShortmailStoryViewController alloc]initWithNibName:@"DAShortmailStoryViewController" bundle:nil];
+        shortmailStoryViewController.hidesBottomBarWhenPushed = YES;
+        
+        DAContact *contact = ((DAContact *)[_notifications objectAtIndex:indexPath.row]);
+        shortmailStoryViewController.contact = contact._id;
+        shortmailStoryViewController.uid = contact.user._id;
+        shortmailStoryViewController.name = contact.user.name.name_zh;
+        
+        [self presentViewController:shortmailStoryViewController animated:YES completion:nil];
+    }
 }
-
 
 @end
